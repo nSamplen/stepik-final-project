2d9f3648f0576525e24420db922b487200b79cf0
Anna Egorova <anyaaaaegorova@gmail.com>
2021-05-04 19:16:03 +0300

    added login_page tests

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,5 @@
 from .base_page import BasePage
-
+from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -9,7 +9,7 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert "login" in self.current_urlself, "Login link is not presented"
+        assert "login" in self.url, "Login link is not presented"
         assert True
 
     def should_be_login_form(self):
