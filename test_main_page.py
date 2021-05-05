8e076e14a40bb0ad87941e3be8cfeb29c433afc1
Anna Egorova <anyaaaaegorova@gmail.com>
2021-05-05 03:09:40 +0300

    login testes gathered in one class with mark login_guest

--- test_main_page.py
+++ test_main_page.py
@@ -2,6 +2,7 @@ from pages.main_page import MainPage
 from pages.login_page import LoginPage
 from pages.basket_page import BasketPage
 from selenium.webdriver.common.by import By
+import pytest
 
 link = "http://selenium1py.pythonanywhere.com/"
 
@@ -34,6 +35,23 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     basket_page.should_be_empty()
     #Ожидаем, что есть текст о том что корзина пуста 
     basket_page.should_be_empty_message()
+
+
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    # не забываем передать первым аргументом self                       
+    def test_guest_can_go_to_login_page(self, browser):     
+        page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+        page.open()                      # открываем страницу
+        page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 
 
